3c377f3874367aab2fb6cde97466dbbe7766e32e
JSurapong <surapong.kawkangploo@gmail.com>
2023-04-05 12:18:20 +0700

    4.1.1 Rule: NEVER USE IF WITH ELSE

--- index.ts
+++ index.ts
@@ -17,11 +17,18 @@ enum Tile {
   LOCK2,
 }
 
-enum Input {
+interface Input {
+  isRight(): boolean;
+  isLeft(): boolean;
+  isUp(): boolean;
+  isDown(): boolean;
+}
+
+enum RawInput {
+  RIGHT,
+  LEFT,
   UP,
   DOWN,
-  LEFT,
-  RIGHT,
 }
 
 let playerx = 1;
@@ -98,22 +105,21 @@ function update() {
 
   function handleInputs() {
     while (inputs.length > 0) {
-      handleInput(inputs.pop())
+      handleInput(inputs.pop());
     }
 
-
     function handleInput(input: Input) {
-      if (input === Input.LEFT) moveHorizontal(-1);
-      else if (input === Input.RIGHT) moveHorizontal(1);
-      else if (input === Input.UP) moveVertical(-1);
-      else if (input === Input.DOWN) moveVertical(1);
+      if (input.isLeft()) moveHorizontal(-1);
+      else if (input.isRight()) moveHorizontal(1);
+      else if (input.isUp()) moveVertical(-1);
+      else if (input.isDown()) moveVertical(1);
     }
   }
 
   function updateMap() {
     for (let y = map.length - 1; y >= 0; y--) {
       for (let x = 0; x < map[y].length; x++) {
-        updateTile(x, y)
+        updateTile(x, y);
       }
     }
 
@@ -136,7 +142,6 @@ function update() {
         map[y][x] = Tile.BOX;
       }
     }
-
   }
 }
 
@@ -145,7 +150,6 @@ function draw() {
 
   drawMap(g);
   drawPlayer(g);
-
 
   function createGraphics() {
     let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
@@ -202,9 +206,70 @@ const LEFT_KEY = "ArrowLeft";
 const UP_KEY = "ArrowUp";
 const RIGHT_KEY = "ArrowRight";
 const DOWN_KEY = "ArrowDown";
+
+class Right implements Input {
+  isRight() {
+    return true;
+  }
+  isLeft() {
+    return false;
+  }
+  isUp() {
+    return false;
+  }
+  isDown() {
+    return false;
+  }
+}
+
+class Left implements Input {
+  isRight() {
+    return false;
+  }
+  isLeft() {
+    return true;
+  }
+  isUp() {
+    return false;
+  }
+  isDown() {
+    return false;
+  }
+}
+
+class Up implements Input {
+  isRight() {
+    return false;
+  }
+  isLeft() {
+    return false;
+  }
+  isUp() {
+    return true;
+  }
+  isDown() {
+    return false;
+  }
+}
+
+class Down implements Input {
+  isRight() {
+    return false;
+  }
+  isLeft() {
+    return false;
+  }
+  isUp() {
+    return false;
+  }
+  isDown() {
+    return true;
+  }
+}
+
 window.addEventListener("keydown", (e) => {
-  if (e.key === LEFT_KEY || e.key === "a") inputs.push(Input.LEFT);
-  else if (e.key === UP_KEY || e.key === "w") inputs.push(Input.UP);
-  else if (e.key === RIGHT_KEY || e.key === "d") inputs.push(Input.RIGHT);
-  else if (e.key === DOWN_KEY || e.key === "s") inputs.push(Input.DOWN);
+  if (e.key === LEFT_KEY || e.key === "a") inputs.push(new Left());
+  else if (e.key === UP_KEY || e.key === "w") inputs.push(new Up());
+  else if (e.key === RIGHT_KEY || e.key === "d") inputs.push(new Right());
+  else if (e.key === DOWN_KEY || e.key === "s") inputs.push(new Down());
 });
