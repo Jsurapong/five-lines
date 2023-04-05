a225f20136150b05ddf6dbac9e62e8911378a8a5
JSurapong <surapong.kawkangploo@gmail.com>
2023-04-05 12:23:58 +0700

    4.1.3 Refactoring pattern: REPLACE TYPE CODE WITH CLASSES step 2

--- index.ts
+++ index.ts
@@ -210,10 +210,7 @@ const DOWN_KEY = "ArrowDown";
 
 class Right implements Input {
   handleInput() {
-    if (false) moveHorizontal(-1);
-    else if (true) moveHorizontal(1);
-    else if (false) moveVertical(-1);
-    else if (false) moveVertical(1);
+    moveHorizontal(1);
   }
   isRight() {
     return true;
