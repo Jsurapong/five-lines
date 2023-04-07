137ce0a12ae5821fe430dfe422ff7d75fa542510
JSurapong <surapong.kawkangploo@gmail.com>
2023-04-07 17:34:54 +0700

    Use drop 103

--- index.ts
+++ index.ts
@@ -600,14 +600,15 @@ function update() {
 
     function updateTile(x: number, y: number) {
       if (map[y][x].isStony() && map[y + 1][x].isAir()) {
-        map[y + 1][x] = new Stone(new Falling());
+        map[y][x].drop();
+        map[y + 1][x] = map[y][x];
         map[y][x] = new Air();
       } else if (
         (map[y][x].isBoxy() || map[y][x].isFallingBox()) &&
         map[y + 1][x].isAir()
       ) {
-        map[y + 1][x] = new Box(new Falling());
-        map[y][x] = new Air();
+        map[y][x].drop();
+        map[y + 1][x] = map[y][x];
       } else if (map[y][x].isFalling()) {
         map[y][x].rest();
       }
